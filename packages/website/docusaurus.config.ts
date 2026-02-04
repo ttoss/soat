a73ad39f31dcc3a43761be1c0f5133743e2703f3
Pedro Arantes <arantespp@gmail.com>
2026-02-04 08:06:23 -0300

    fix: docs layout

--- packages/website/docusaurus.config.ts
+++ packages/website/docusaurus.config.ts
@@ -113,16 +113,16 @@ const config: Config = {
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
-          position: 'left',
+          position: 'right',
           label: 'Docs',
         },
         {
           type: 'docSidebar',
           sidebarId: 'apiSidebar',
-          position: 'left',
+          position: 'right',
           label: 'API',
         },
-        { to: '/blog', label: 'Blog', position: 'left' },
+        { to: '/blog', label: 'Blog', position: 'right' },
         {
           href: 'https://github.com/ttoss/soat',
           label: 'GitHub',
